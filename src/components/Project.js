e065e0df691789edf4173abe071218d61425b3e5
saundersp <pierre.saundersgb@gmail.com>
2020-08-15 23:59:19 +0200

    added download button if project has download url

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import Fade from 'react-reveal/Fade';
-import { FaGithub, FaRegPaperPlane, FaRegImage } from 'react-icons/fa';
+import { FaGithub, FaRegPaperPlane, FaRegImage, FaDownload } from 'react-icons/fa';
 import '../scss/Project.scss';
 import language from '../data/info';
-const { liveDemo } = language.menu;
+const { liveDemo, download_link } = language.menu;
 
 export default class Project extends Component {
 	state = {
@@ -13,7 +13,7 @@ export default class Project extends Component {
 	imgNotFound = _ => this.setState({ imageLoadingError: true });
 
 	render() {
-		const { url, imageSrc, title, service, desc, src, techs } = this.props.project;
+		const { url, imageSrc, title, service, desc, src, techs, download_url } = this.props.project;
 		const image = imageSrc ?
 			<img decoding="async" src={imageSrc} alt={title} onError={this.imgNotFound} />
 			: <this.props.project.imageIcon />;
@@ -33,6 +33,7 @@ export default class Project extends Component {
 					</ul>
 					<ul className="links">
 						{url && (<li><a target='_blank' rel="noopener noreferrer" href={url}><FaRegPaperPlane /> {liveDemo[this.props.selectedLang]}</a></li>)}
+						{download_url && (<li><a target='_blank' rel="noopener noreferrer" href={download_url}><FaDownload /> {download_link[this.props.selectedLang]}</a></li>)}
 						{src && (<li><a target='_blank' rel="noopener noreferrer" href={src}><FaGithub /> Source</a></li>)}
 					</ul>
 				</div>
