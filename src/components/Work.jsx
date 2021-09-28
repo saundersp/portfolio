09d4168497f9680bcdf0f63e8b5d2c93bcee10fe
saundersp <pierre.saundersgb@gmail.com>
2021-09-28 18:02:40 +0200

    Divided projects into sub categories

---
+++ src/components/Work.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Fade } from 'react-awesome-reveal';
+import '../scss/Work.scss';
+import Project from './Project';
+import info from '../data/info';
+import { useLang } from '../services/LanguageService';
+
+export default function Work() {
+
+	const { t, getLangIndex } = useLang();
+
+	return (
+		<Fade direction="left" duration={500} cascade triggerOnce>
+			<div className='work'>
+				<h1 className='heading'>{t('menu.work')}</h1>
+				{info.projects_title[getLangIndex()].reduce((tab, key, i) => {
+					tab.push(<h2 key={'h' + key}>{key}</h2>);
+					tab.push(<div key={'w' + key} className='work-content'>
+						{info.projects[i].map((project, index) =>
+							<Project key={index} project={project}></Project>
+						)}
+					</div>);
+					return tab;
+				}, [])}
+			</div>
+		</Fade>
+	);
+}
